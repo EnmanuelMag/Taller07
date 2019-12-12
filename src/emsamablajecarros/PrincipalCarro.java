7577f281f44f1a26e1cb885b44b85aed506cb696
Enmanuel Magallanes <emmanu2006@hotmail.com>
2019-12-12 13:10:27 -0500

    Correcion

--- src/emsamablajecarros/PrincipalCarro.java
+++ src/emsamablajecarros/PrincipalCarro.java
@@ -13,7 +13,7 @@ import emsamablajecarro.Builder.ChevroletSparktBuilder;
  * @author djurado
  *
  */
-public class PrincipalComputadoras {
+public class PrincipalCarro {
 
 	/**
 	 * @param args
@@ -22,21 +22,15 @@ public class PrincipalComputadoras {
 		
 		
 		CarroDirector cD = new CarroDirector(new ChevroletSailBuilder());
-		
 		cD.contruirCarro();
-		
 		Carro sail = cD.getCarro();
 		
 		CarroDirector cD2 = new CarroDirector(new ChevroletSparktBuilder());
-		
 		cD2.contruirCarro();
-		
 		Carro sparkt = cD2.getCarro();
                 
                 CarroDirector cD3 = new CarroDirector(new ChevroletAveoBuilder());
-		
 		cD3.contruirCarro();
-		
 		Carro aveo = cD3.getCarro();
 		
 		//Mostrar las caracteristicas de ambas computadoras 
